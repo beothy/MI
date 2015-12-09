fc51cd5cb80b0838b3a25352d71cbb200e341e9e
Beöthy Bence <beothy.bence@gmail.com>
2015-12-09 16:47:51 +0100

    loadFile implementálás

--- src/mi/Kmean.java
+++ src/mi/Kmean.java
@@ -1,5 +1,79 @@
 package mi;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Kmean implements Modell {
+	public ArrayList<Item> items = new ArrayList<Item>(); 
+
+	@Override
+	public void kmean() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean loadFile(String path) {
+		// TODO Auto-generated method stub
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(path));
+				String line = br.readLine();
+				line = br.readLine();
+				
+				while(line != null){
+					String[] params = line.split("\t");
+					
+					Item item = new Item();
+					item.setWidth(Double.parseDouble(params[1]));
+					item.setHeight(Double.parseDouble(params[2]));
+					item.setArea(Double.parseDouble(params[3]));
+					item.setPerimeter(Double.parseDouble(params[4]));
+					item.setShapeFactor(Double.parseDouble(params[5]));
+					item.setAreaLoadFactor(Double.parseDouble(params[6]));
+					
+					items.add(item);
+					
+					line = br.readLine();
+				}
+						
+					
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Kmean [items=" + items + "]";
+	}
+
+	@Override
+	public ArrayList<Item> getList() {
+		// TODO Auto-generated method stub
+		return items;
+	}
 
 }
--- src/mi/Modell.java
+++ src/mi/Modell.java
@@ -1,5 +1,11 @@
 package mi;
 
-public interface Modell {
+import java.util.ArrayList;
 
+public interface Modell {
+	public ArrayList<Item> getList();
+	public void kmean();
+	public void step();
+	public void run();
+	public boolean loadFile(String path);
 }
