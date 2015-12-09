c932db35123b0f5b73fe900cbe3211faf38b60df
Beöthy Bence <beothy.bence@gmail.com>
2015-12-09 14:54:30 +0100

    All parameter - setter,getter

--- src/mi/Item.java
+++ src/mi/Item.java
@@ -8,18 +8,83 @@ package mi;
  *
  */
 public class Item {
-	private double paramA;
-	private double paramB;
-	public double getParamA() {
-		return paramA;
+	//Width	Height	Area	Perimeter	ShapeFactor	AreaLoadFactor	
+	private double width;
+	private double height;
+	private double area;
+	private double perimeter;
+	private double shapeFactor;
+	private double areaLoadFactor;
+	/**
+	 * @return the width
+	 */
+	public double getWidth() {
+		return width;
 	}
-	public void setParamA(double paramA) {
-		this.paramA = paramA;
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(double width) {
+		this.width = width;
 	}
-	public double getParamB() {
-		return paramB;
+	/**
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
 	}
-	public void setParamB(double paramB) {
-		this.paramB = paramB;
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	/**
+	 * @return the area
+	 */
+	public double getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(double area) {
+		this.area = area;
+	}
+	/**
+	 * @return the perimeter
+	 */
+	public double getPerimeter() {
+		return perimeter;
+	}
+	/**
+	 * @param perimeter the perimeter to set
+	 */
+	public void setPerimeter(double perimeter) {
+		this.perimeter = perimeter;
+	}
+	/**
+	 * @return the shapeFactor
+	 */
+	public double getShapeFactor() {
+		return shapeFactor;
+	}
+	/**
+	 * @param shapeFactor the shapeFactor to set
+	 */
+	public void setShapeFactor(double shapeFactor) {
+		this.shapeFactor = shapeFactor;
+	}
+	/**
+	 * @return the areaLoadFactor
+	 */
+	public double getAreaLoadFactor() {
+		return areaLoadFactor;
+	}
+	/**
+	 * @param areaLoadFactor the areaLoadFactor to set
+	 */
+	public void setAreaLoadFactor(double areaLoadFactor) {
+		this.areaLoadFactor = areaLoadFactor;
 	}
 }
