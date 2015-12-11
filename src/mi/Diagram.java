d60251ca61d0727ee0c0958b076cc86e57037c57
Beöthy Bence <beothy.bence@gmail.com>
2015-12-11 13:24:54 +0100

    few change in draw

--- src/mi/Diagram.java
+++ src/mi/Diagram.java
@@ -65,17 +65,17 @@ public class Diagram extends View{
 	 public void init(Graphics g) {
 		 Graphics2D g2d = (Graphics2D) g.create();
 		 
-		 for(int i = 0; i<7;i++)
+		 for(int i = 1; i<6;i++)
 			 g2d.drawRect(15+i*100, 15, 100, 500);
 		 
 		 g2d.setFont(new Font("TimesRoman", Font.PLAIN, 15));
 		 g2d.drawString("Properties:", 15, 535);
-		 g2d.drawString("Width", 100, 535);
+		 g2d.drawString("Width", 93, 535);
 		 g2d.drawString("Height", 200, 535);
 		 g2d.drawString("Area", 300, 535);
-		 g2d.drawString("Perimeter", 400, 535);
-		 g2d.drawString("ShapeFactor", 500, 535);
-		 g2d.drawString("AreaLoadFactor", 600, 535);
+		 g2d.drawString("Perimeter", 385, 535);
+		 g2d.drawString("ShapeFactor", 478, 535);
+		 g2d.drawString("AreaLoadFactor", 570, 535);
 		 
 		 for(int i = 0; i < 360; i += 360/numOfCL) {
 			   
